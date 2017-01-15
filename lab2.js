8aba5047b6f7db9e61a521ae83d6eace7f1d9c66
Wh4tever <AkishinVA@gmail.com>
2017-01-15 19:06:42 +0700

    Yeah

---
+++ lab2.js
@@ -0,0 +1,25 @@
+﻿var xhr = new XMLHttpRequest();
+xhr.open('GET', 'access.html', false);
+xhr.send();
+var regExpIP = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/ig;
+var IPList = xhr.responseText.match(regExpIP);
+var d = {};
+for (var i in IPList) {
+    if (IPList[i] in d) {
+        d[IPList[i]] += 1;
+    }
+    else d[IPList[i]] = 1;
+}
+var subnet = "";
+var keys = Object.keys(d);
+keys.sort();
+for (var i in keys) {
+    var regExpSubnet = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/i;
+    var sn = keys[i].match(regExpSubnet);
+    if (sn[0] != subnet) { 
+        console.log(); 
+        console.log(sn[0]);
+        subnet = sn[0]; 
+    }
+    console.log(keys[i], "-", d[keys[i]]);
+}
